5c08613f2166c6bfd2a50c6b7078dfc529ea5942
Bruno Roja <brunojlee@hotmail.com>
2023-02-06 11:27:49 -0400

    feat: :sparkles: New assertion on foods page
    Signed-off-by: Bruno Roja <brunojlee@hotmail.com>

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -8,5 +8,6 @@ describe('SwapCard', () => {
     cy.get('input[name="password"]').type('12345678')
     cy.get('button[type="button"]').click()
     cy.url().should('include', '/foods')
+		cy.get('[data-testid=0-card-name]').should('have.text', 'Corba')
   })    
 })
