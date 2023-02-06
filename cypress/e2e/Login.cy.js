190c8ca1e524148e7c1e48959728045c22ba4a32
Bruno Roja <brunojlee@hotmail.com>
2023-02-06 14:27:28 -0400

    feat: :sparkles: Tests and env changes
    Signed-off-by: Bruno Roja <brunojlee@hotmail.com>

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,13 +1,13 @@
-describe('SwapCard', () => {
-	beforeEach(() => {
-		cy.visit('/')
-	})
+describe('LoginCard', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
   it('should login', () => {
-    cy.get('input[name="email"]').type('teste@teste.com')
-    cy.get('input[name="password"]').type('12345678')
-    cy.get('button[type="button"]').click()
-    cy.url().should('include', '/foods')
-		cy.get('[data-testid=0-card-name]').should('have.text', 'Corba')
-  })    
-})
+    cy.get('input[name="email"]').type('teste@teste.com');
+    cy.get('input[name="password"]').type('12345678');
+    cy.get('button[type="button"]').click();
+    cy.url().should('include', '/foods');
+    cy.get('[data-testid=0-card-name]').should('have.text', 'Corba');
+  });
+});
